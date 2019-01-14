81fff01b181dfb0fe761e78a340fe8d54e505e10
Siva Selvaraj <SSelvaraj@objectconsulting.com.au>
2019-01-14 14:09:25 +1100

    port changed

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 
 var publicPath = path.join(__dirname, '../public')
 var app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
 // this is used to configure the express app middleware
 app.use(express.static(publicPath))
