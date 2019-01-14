c8d7397e3c32c72e5d3ce75975689a4a594f978e
Siva Selvaraj <SSelvaraj@objectconsulting.com.au>
2019-01-14 13:54:40 +1100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express')
+
+var publicPath = path.join(__dirname, '../public')
+var app = express()
+const port = process.env.PORT || 3000
+
+// this is used to configure the express app middleware
+app.use(express.static(publicPath))
+
+app.listen(port, () => {
+    console.log(`Server is up and running in the PORT ${port}`)
+})
+
+console.log(__dirname + '/../public')
+console.log(publicPath)
