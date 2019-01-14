fa44f4397bd3ddfa628dc24d8e398a2f75ccbdf0
Siva Selvaraj <SSelvaraj@objectconsulting.com.au>
2019-01-15 10:55:20 +1100

    client and server socket IO connection

--- server/server.js
+++ server/server.js
@@ -1,16 +1,30 @@
 const path = require('path')
 const express = require('express')
+const socketIO = require('socket.io')
+const http = require('http')
 
 var publicPath = path.join(__dirname, '../public')
-var app = express()
+
 const port = process.env.PORT || 3000
+
+var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
 
 // this is used to configure the express app middleware
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
-    console.log(`Server is up and running in the PORT ${port}`)
+// Register an client event
+io.on('connection', (socket) => {
+    console.log('New user client connected')
+
+
+    socket.on('disconnect', () => {
+        console.log('User client disconnected')
+    })
+
 })
 
-console.log(__dirname + '/../public')
-console.log(publicPath)
+server.listen(port, () => {
+    console.log(`Server is up and running in the PORT ${port}`)
+})
